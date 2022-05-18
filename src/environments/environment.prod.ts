968dec46e112baedc672af82f9b16aa0644809be
Erick Martinez Gomez <new_angel_24@hotmail.com>
2022-05-18 15:08:34 -0500

    cambio en datos de conexion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: "https://localhost:44365",
-  //apiUrl: "http://52.242.246.67:5000"
+  //apiUrl: "https://localhost:44365",
+  apiUrl: "http://52.242.246.67:5000"
 };
